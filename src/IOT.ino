46b8a74a3a94aa41e873f434c24f6db0aa3f6e08
Lukas Schröer <lukas.schroeer@gmx.at>
2019-09-28 00:56:48 +0200

    home assistant discovery, changed to PubSubMqtt

--- src/IOT.ino
+++ src/IOT.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
-#include "Adafruit_MQTT.h"
-#include "Adafruit_MQTT_Client.h"
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
@@ -18,6 +18,22 @@ void coPwmCallback();
 void mqttServiceCallback();
 void sensorCallback();
 void blinkled();
+
+typedef enum sensors_e{
+  MQ3,
+  MQ4,
+  MQ7,
+  MQ8,
+  MQ135,
+  LIGHT,
+  UV,
+  DUST,
+  SOUND,
+  TEMP,
+  HUM,
+  PRES
+}sensors_t;
+
 
 /************************* Tasks *********************************/
 
@@ -46,68 +62,238 @@ IoAbstractionRef ioExpander = ioFrom8574(ADDR_PEX);
 
 unsigned int data_chache[16][5];
 float data_bme[3];
-bool  control_reg[1] = {0}; //{bme enable bit, 
+bool  control_reg[1] = {0}; //{bme enable bit,
 bool ready = 0;
 
 /************************* WiFi Access Point *********************************/
 
-#define WLAN_SSID "***REMOVED***"
-#define WLAN_PASS "***REMOVED***"
+const char* ssid = "***REMOVED***";
+const char* password = "***REMOVED***";
+const char* mqtt_server = "***REMOVED***";
+const char* clientUsr = "***REMOVED***";
+const char* clientPw = "***REMOVED***";
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
 
-/************************* Adafruit.io Setup *********************************/
+String BOARDNAME = "senbrd1";
+String MTYPE = "sensor";
+String HOMEPREFIX = "homeassistant";
+String UUID = (String)ESP.getChipId();
+String VERSION = "V2.0";
 
-#define AIO_SERVER      "***REMOVED***"
-#define AIO_SERVERPORT  1883                   // use 8883 for SSL
-#define AIO_USERNAME    ""
-#define AIO_KEY         ""
+String MQ3_topic = "MQ3";
+String MQ4_topic = "MQ4";
+String MQ7_topic = "MQ7";
+String MQ8_topic = "MQ8";
+String MQ135_topic = "MQ135";
+String LIGHT_topic = "LIGHT";
+String UV_topic = "UV";
+String DUST_topic = "DUST";
+String SOUND_topic = "SOUND";
+String TEMP_topic = "TEMP";
+String HUM_topic = "HUM";
+String PRES_topic = "PRES";
+String sensor_suffix[] ={
+  MQ3_topic,
+  MQ4_topic,
+  MQ7_topic,
+  MQ8_topic,
+  MQ135_topic,
+  LIGHT_topic,
+  UV_topic,
+  DUST_topic,
+  SOUND_topic,
+  TEMP_topic,
+  HUM_topic,
+  PRES_topic
+};
+String sensor_unit[] ={
+  "V",
+  "V",
+  "V",
+  "V",
+  "V",
+  "V",
+  "V",
+  "V",
+  "V",
+  "°C",
+  "%",
+  "hPa"
+};
 
-/************ Global State (you don't need to change this!) ******************/
-
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
-WiFiClient client;
-// or... use WiFiFlientSecure for SSL
-//WiFiClientSecure client;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, AIO_SERVER, AIO_SERVERPORT, AIO_USERNAME, AIO_KEY);
-
-/****************************** Feeds ***************************************/
-
-// Setup a feed called 'photocell' for publishing.
-// Notice MQTT paths for AIO follow the form: <username>/feeds/<feedname>
-#define BOARDNAME "senbrd1"
-Adafruit_MQTT_Publish MQ3 =   Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/MQ3");
-Adafruit_MQTT_Publish MQ4 =   Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/MQ4");
-Adafruit_MQTT_Publish MQ7 =   Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/MQ7");
-Adafruit_MQTT_Publish MQ8 =   Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/MQ8");
-Adafruit_MQTT_Publish MQ135 = Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/MQ135");
-Adafruit_MQTT_Publish LIGHT = Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/LIGHT");
-Adafruit_MQTT_Publish UV =    Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/UV");
-Adafruit_MQTT_Publish DUST =  Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/DUST");
-Adafruit_MQTT_Publish SOUND = Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/SOUND");
-Adafruit_MQTT_Publish TEMP =  Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/TEMP");
-Adafruit_MQTT_Publish HUM =   Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/HUM");
-Adafruit_MQTT_Publish PRES =  Adafruit_MQTT_Publish(&mqtt, "/" BOARDNAME "/PRES");
 
 
-void setup(void) 
-{
-  
-  Serial.begin(115200);
+//https://arduinojson.org/v6/assistant/
+
+void switchDiscovery(){
+  char topic_buffer[50], payload_buffer[300];
+  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(12);
+  DynamicJsonDocument doc(capacity);
+
+  doc["~"] = BOARDNAME + "/";
+  doc["name"] = BOARDNAME;
+  doc["cmd_t"] = "~cmnd/POWER";
+  doc["stat_t"] = "~tele/STATE";
+  //doc["val_tpl"] = "{{value_json.POWER}}";
+  doc["pl_off"] = "OFF";
+  doc["pl_on"] = "ON";
+  doc["avty_t"] = "~tele/LWT";
+  doc["pl_avail"] = "Online";
+  doc["pl_not_avail"] = "Offline";
+  doc["uniq_id"] = UUID + "_switch";
+
+  JsonObject device = doc.createNestedObject("device");
+  JsonArray device_identifiers = device.createNestedArray("identifiers");
+  device_identifiers.add(UUID);
+
+  serializeJson(doc, payload_buffer);
+  sprintf(topic_buffer, "%s/%s/%s/%s", HOMEPREFIX.c_str(), "switch",(UUID + "_switch").c_str(), "config");
+  client.publish(topic_buffer, payload_buffer, strlen(payload_buffer));
+}
+
+void statusDiscovery(){
+  char topic_buffer[50], payload_buffer[1000];
+  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(11);
+  DynamicJsonDocument doc(capacity);
+
+  doc["name"] = BOARDNAME + " status";
+  doc["stat_t"] = "~HASS_STATE";
+  doc["avty_t"] = "~LWT";
+  doc["pl_avail"] = "Online";
+  doc["pl_not_avail"] = "Offline";
+  doc["json_attributes_topic"] = "~HASS_STATE";
+  doc["unit_of_meas"] = " ";
+  doc["val_tpl"] = "{{value_json['RSSI']}}";
+  doc["uniq_id"] = UUID + "_status";
+
+  JsonObject device = doc.createNestedObject("device");
+  JsonArray device_identifiers = device.createNestedArray("identifiers");
+  device_identifiers.add(UUID);
+  device["name"] = BOARDNAME;
+  device["model"] = "DIY Sensorboard";
+  device["sw_version"] = VERSION;
+  device["manufacturer"] = "Lukas Schröer";
+  doc["~"] = BOARDNAME + "/tele/";
+
+  serializeJson(doc, payload_buffer);
+  strcpy(payload_buffer,"{\"name\":\"senbrd1 status\",\"stat_t\":\"~HASS_STATE\",\"avty_t\":\"~LWT\",\"pl_avail\":\"Online\",\"pl_not_avail\":\"Offline\",\"json_attributes_topic\":\"~HASS_STATE\",\"unit_of_meas\":\" \",\"val_tpl\":\"{{value_json['RSSI']}}\",\"uniq_id\":\"3998661_status\",\"device\":{\"identifiers\":[\"3998661\"],\"name\":\"senbrd1\",\"model\":\"DIY Sensorboard\"}}");
+  sprintf(topic_buffer, "%s/%s/%s/%s", HOMEPREFIX.c_str(), "sensor", (UUID + "_status").c_str(), "config");
+  Serial.println(topic_buffer);
+  Serial.println(payload_buffer);
+  client.publish(topic_buffer, payload_buffer, strlen(payload_buffer));
+}
+
+void hassStatePublish(){
+  char topic_buffer[50], payload_buffer[1000];
+  const size_t capacity = JSON_OBJECT_SIZE(15);
+  DynamicJsonDocument doc(capacity);
+
+  doc["Version"] = VERSION;
+  doc["BuildDateTime"] = __TIME__;
+  doc["Module"] = BOARDNAME;
+  doc["Uptime"] = millis();
+  doc["IPAddress"] = WiFi.localIP();
+  doc["RSSI"] = WiFi.RSSI();
+
+  serializeJson(doc, payload_buffer);
+  sprintf(topic_buffer, "%s/%s/%s", HOMEPREFIX.c_str(), "tele", "HASS_STATE");
+  client.publish(topic_buffer, payload_buffer, strlen(payload_buffer));
+}
+
+void sensorConfigDiscovery(String name, String unit){
+  char topic_buffer[50], payload_buffer[1000];
+  String uniqid = UUID + name;
+  uniqid.replace(' ', '_');
+  const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(10);
+  DynamicJsonDocument doc(capacity);
+
+  doc["name"] = name;
+  doc["stat_t"] = "~" + name;
+  doc["avty_t"] = "~LWT";
+  doc["pl_avail"] = "Online";
+  doc["pl_not_avail"] = "Offline";
+  doc["uniq_id"] = uniqid;
+
+  JsonObject device = doc.createNestedObject("device");
+  JsonArray device_identifiers = device.createNestedArray("identifiers");
+  device_identifiers.add(UUID);
+  doc["~"] = BOARDNAME + "/tele/";
+  doc["unit_of_meas"] = unit;
+
+  serializeJson(doc, payload_buffer);
+  sprintf(topic_buffer, "%s/%s/%s", HOMEPREFIX.c_str(), "sensor", uniqid.c_str(), "config");
+  client.publish(topic_buffer, payload_buffer, strlen(payload_buffer));
+}
+
+void setupDiscovery(){
+  switchDiscovery();
+  statusDiscovery();
+  for(int i=0; i<sizeof(sensor_suffix)/sizeof(sensor_suffix[0]); i++){
+    sensorConfigDiscovery(sensor_suffix[i], sensor_unit[i]);
+  }
+}
+
+
+void setup_wifi() {
+
   delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
   Serial.print("Connecting to ");
-  Serial.println(WLAN_SSID);
+  Serial.println(ssid);
 
-  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  WiFi.begin(ssid, password);
+
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
 
+  randomSeed(micros());
+
+  Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: "); Serial.println(WiFi.localIP());
-  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), clientUsr, clientPw)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  reconnect();
+  setupDiscovery();
+
   Wire.begin();
 
   ioDevicePinMode(ioExpander, 0, OUTPUT);
@@ -125,7 +311,7 @@ void setup(void)
   ioDeviceDigitalWrite(ioExpander, 2, 0); //C
   ioDeviceDigitalWrite(ioExpander, 3, 0); //EN, ACTIVE LOW
   ioDeviceDigitalWrite(ioExpander, 4, 1); //EN, ACTIVE LOW
-  
+
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
   // exceed the upper and lower limits if you adjust the input range!
@@ -139,8 +325,7 @@ void setup(void)
   // adc.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // adc.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
-  pinMode(2, OUTPUT);
-  digitalWrite(2, 1);
+  digitalWrite(LED_BUILTIN, 1);
 
   Wire.beginTransmission(ADDR_PEX);
   if (Wire.endTransmission()!=0) {
@@ -174,50 +359,27 @@ void setup(void)
                   Adafruit_BME280::FILTER_X16,
                   Adafruit_BME280::STANDBY_MS_0_5 );
   }
-  
-  
+
+
   t5.delay(15001);
-  runner.startNow(); 
+  runner.startNow();
 }
 
 
 void loop(){
-
-  MQTT_connect();
+  if (!client.connected()) {
+     reconnect();
+   }
+  client.loop();
   ioDeviceSync(ioExpander);
   runner.execute();
-  
+
 }
 
 void blinkled(){
   static bool state = 1;
   state = !state;
-  digitalWrite(2, state);
-}
-
-void MQTT_connect() {
-  int8_t ret;
-
-  // Stop if already connected.
-  if (mqtt.connected()) {
-    return;
-  }
-
-  Serial.print("Connecting to MQTT... ");
-
-  uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
-       Serial.println(mqtt.connectErrorString(ret));
-       Serial.println("Retrying MQTT connection in 5 seconds...");
-       mqtt.disconnect();
-       delay(5000);  // wait 5 seconds
-       retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
-       }
-  }
-  Serial.println("MQTT Connected!");
+  digitalWrite(LED_BUILTIN, state);
 }
 
 void dustPwmCallback(){
@@ -234,11 +396,11 @@ void dustPwmCallback(){
   //ioDeviceDigitalWrite(ioExpander, 6, 1);
   digitalWrite(D5, 1);
   ioDeviceSync(ioExpander);
-  
+
   delayMicroseconds(280);
   data_chache[7][dim] = adc.readADC_SingleEnded(0);
   delayMicroseconds(40);
-  
+
   //ioDeviceDigitalWrite(ioExpander, 6, 0);
   digitalWrite(D5, 0);
   ioDeviceSync(ioExpander);
@@ -248,7 +410,7 @@ void dustPwmCallback(){
 void sensorCallback(){
 
   #define LAST_SENSOR 14
-  
+
   static int dim = -1;
   dim++;
   if(dim==5){
@@ -281,43 +443,64 @@ void sensorCallback(){
   if(control_reg[0]){
     data_bme[0] = bme.readTemperature();
     data_bme[1] = bme.readPressure()/100;
-    data_bme[2] = bme.readHumidity();  
+    data_bme[2] = bme.readHumidity();
   }
 
-  
+
 }
 
 void mqttServiceCallback(){
   bool state = 0;
-  float data_chache_sum[16];
+  float data_cache_sum[16];
 
   if(!ready) return;
 
   for(int i=0; i<16; i++){
-    data_chache_sum[i]=0;
+    data_cache_sum[i]=0;
     for(int j=0; j<5; j++){
       if(data_chache[i][j] <= 0x7FFF)
-        data_chache_sum[i] += data_chache[i][j] * 0.0001875;
+        data_cache_sum[i] += data_chache[i][j] * 0.0001875;
       else
-        data_chache_sum[i] += ((float)data_chache[i][j] - (float)0xFFFF) * 0.0001875;
+        data_cache_sum[i] += ((float)data_chache[i][j] - (float)0xFFFF) * 0.0001875;
     }
-    data_chache_sum[i] = data_chache_sum[i]/5;
+    data_cache_sum[i] = data_cache_sum[i]/5;
   }
 
-  MQ3.publish(data_chache_sum[0], 5);
-  MQ4.publish(data_chache_sum[1], 5);
-  MQ7.publish(data_chache_sum[2], 5);
-  MQ8.publish(data_chache_sum[3], 5);
-  MQ135.publish(data_chache_sum[4], 5);
-  LIGHT.publish(data_chache_sum[5], 5);
-  UV.publish(data_chache_sum[6], 5);
-  DUST.publish(data_chache_sum[7], 5);
-  SOUND.publish(data_chache_sum[8], 5);
+  char topic[50], payload[10];
+  for(int i=0; i<(sizeof(sensor_suffix)/sizeof(sensor_suffix[0])-3); i++){
+    snprintf(payload, sizeof(payload), "%.5f", data_cache_sum[i]);
+    strcpy(topic,(BOARDNAME + "/tele/" + sensor_suffix[i]).c_str());
+    //snprintf(topic, sizeof(topic), "%s", tmp);
+    client.publish(topic, payload);
+  }
+
+  if(control_reg[0]){
+    snprintf(payload, sizeof(payload), "%.2f", data_bme[0]);
+    strcpy(topic,(BOARDNAME + "/tele/" + sensor_suffix[TEMP]).c_str());
+    client.publish(topic, payload);
+    snprintf(payload, sizeof(payload), "%.2f", data_bme[1]);
+    strcpy(topic,(BOARDNAME + "/tele/" + sensor_suffix[PRES]).c_str());
+    client.publish(topic, payload);
+    snprintf(payload, sizeof(payload), "%.2f", data_bme[2]);
+    strcpy(topic,(BOARDNAME + "/tele/" + sensor_suffix[HUM]).c_str());
+    client.publish(topic, payload);
+  }
+  hassStatePublish();
+  /*
+  MQ3.publish(data_cache_sum[0], 5);
+  MQ4.publish(data_cache_sum[1], 5);
+  MQ7.publish(data_cache_sum[2], 5);
+  MQ8.publish(data_cache_sum[3], 5);
+  MQ135.publish(data_cache_sum[4], 5);
+  LIGHT.publish(data_cache_sum[5], 5);
+  UV.publish(data_cache_sum[6], 5);
+  DUST.publish(data_cache_sum[7], 5);
+  SOUND.publish(data_cache_sum[8], 5);
   if(control_reg[0]){
     TEMP.publish(data_bme[0]);
     PRES.publish(data_bme[1]);
     HUM.publish(data_bme[2]);
   }
-    
-   
+  */
+
 }
